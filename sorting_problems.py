3b37129ae0907b382539a29fa5d956792dd7b1f2
alexanderfeitler <afeitler21@fwparker.org>
2019-03-15 19:13:15 -0500

    Newest

--- sorting_problems.py
+++ sorting_problems.py
@@ -35,15 +35,17 @@ print()
 print("Problem 3\n")
 sort_me2 = [551, 138, 802, 954, 569, 372, 454, 366, 936, 959, 958, 202, 474, 658, 108, 424, 523, 611, 557, 0, 733, 903, 788, 850, 11, 12, 975]
 print(sort_me2)
-for key_pos in range(1, len(sort_me2)):
-    key_value = sort_me2[key_pos]
-    scan_pos = key_pos - 1
-    while (scan_pos >= 0) and (sort_me2[scan_pos] > key_value):
-        sort_me2[scan_pos + 1] = sort_me2[scan_pos]
-        scan_pos -= 1
+def insertion(sort_me2):
+    for key_pos in range(1, len(sort_me2)):
+        key_value = sort_me2[key_pos]
+        scan_pos = key_pos - 1
+        while (scan_pos >= 0) and (sort_me2[scan_pos] > key_value):
+            sort_me2[scan_pos + 1] = sort_me2[scan_pos]
+            scan_pos -= 1
 
 
         sort_me2[scan_pos + 1] = key_value
+sort(sort_me2)
 print(sort_me2)
 print()
 # Problem 4 - Efficiency? (10 pts)
@@ -81,10 +83,11 @@ print("4b")
 sort_me4 = [50, 61, 96, 72, 67, 12, 14, 1, 35, 51, 38, 32, 34, 29, 95, 75, 74, 83, 33, 3, 70, 0, 41, 4, 32, 1, 93, 39, 4, 20, 14, 11, 24, 69, 36, 36, 54, 90, 95, 36, 25, 24, 76, 30, 92, 95, 24, 6, 72, 78, 95, 73, 94, 33, 36, 30, 19, 23, 52, 28, 17, 82, 98, 74, 67, 43, 2, 89, 87, 8, 91, 7, 22, 78, 74, 84, 74, 87, 67, 93, 47, 74, 95, 92, 25, 46, 8, 74, 58, 80, 33, 31, 69, 2, 21, 93, 96, 72, 50, 61]
 
 
+print(sort_me4)
 def insertion(sort_me4):
     loop1 = 0
     loop2 = 0
-    for key_pos in range(1, len(sort_me4)):
+    for key_pos in range(1, len(sort_me2)):
         key_value = sort_me4[key_pos]
         scan_pos = key_pos - 1
         loop1 += 1
@@ -92,10 +95,13 @@ def insertion(sort_me4):
             sort_me4[scan_pos + 1] = sort_me4[scan_pos]
             scan_pos -= 1
             loop2 += 1
-    print(loop1)
-    print(loop2)
 
-    # everything has shifted to make room for the key value
-    sort_me4[scan_pos + 1] = key_value
+        sort_me4[scan_pos + 1] = key_value
+
+    print("Inner loop:", loop1)
+    print("Outer loop:", loop2)
+
+
+sort(sort_me4)
 print(sort_me4)
 print()
